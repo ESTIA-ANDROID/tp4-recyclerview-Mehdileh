00db44cb0c0a7d695abccf9c826139e42d6c4ccf
mehdi.lehjaj <mehdi.lehjaj@etu.estia.fr>
2025-01-29 14:48:26 +0100

    Suppression du projet, ne garder que le README

--- app/src/main/java/com/openclassrooms/magicgithub/api/ApiService.kt
+++
@@ -1,9 +0,0 @@
-package com.openclassrooms.magicgithub.api
-
-import com.openclassrooms.magicgithub.model.User
-
-interface ApiService {
-    fun getUsers(): List<User>
-    fun addRandomUser()
-    fun deleteUser(username: User)
-}
--- app/src/main/java/com/openclassrooms/magicgithub/model/User.kt
+++
@@ -1,7 +0,0 @@
-package com.openclassrooms.magicgithub.model
-
-data class User(
-    val id: String,
-    val login: String,
-    val avatarUrl: String
-)
